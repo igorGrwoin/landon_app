79b0c4ea1e960bb4714043e2f6ccb87914a7e0fc
Igor Silva <igor.silva9813@gmail.com>
2019-05-15 14:09:42 +0000

    added encrypt,decrypt and query with facades

--- landon_app/routes/web.php
+++ landon_app/routes/web.php
@@ -11,11 +11,13 @@
 |
 */
 
+//Main page text
 Route::get('/', function () {
     return '<h3> Landon App Page </h3>';
     #return view('welcome');
 });
 
+//About page shows json text
 Route::get('/about', function () {
     $response_arr = [];
     $response_arr['author'] = 'BP';
@@ -25,11 +27,33 @@ Route::get('/about', function () {
     #return view('welcome');
 });
 
+//home page shows data loaded in variable
 Route::get('/home', function () {
     $data = [];
     $data['version'] = '0.1.1';
     return view('welcome', $data);
 });
 
+//di page shows dependecy injection example 
 Route::get('/di', 'ClientController@di');
+
+//facades/db shows a simulation of db connection
+Route::get('/facades/db', function () {
+    return DB::select('SELECT * from table');
+    //error no db configured
+});
+
+//Shous encypt with facades
+Route::get('/facades/encrypt', function () {
+    return Crypt::encrypt('123456789');
+});
+
+//Shows decrypt with facades
+Route::get('/facades/decrypt', function () {
+    return Crypt::decrypt('eyJpdiI6Im5Hc2FvZGJnUXVxZWxYN3Zab0djQkE9PSIsInZhbHVlIjoiTktsVlNFcUlBWTdYbzVPcHR2Z1VDdWRCWlVhZE5DSUVWWk9SMkdWT2V5Zz0iLCJtYWMiOiI3OGQ3ZmU5ZWRlZGQ3YzJlOTJjMmNhODE0ZThmNTc0ZmEzZmQ1ZTIwYzQyMzVlYWJhMjdiYzI1NjI4MmE1NTgyIn0=');
+});
+
+
+
+
 
