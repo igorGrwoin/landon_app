88e4c71b80c7965a10f82899adf78234c1639fdc
Igor Silva <igor.silva9813@gmail.com>
2019-05-20 15:23:49 +0000

    Database migrations, Models in Laravel, Add data with Eloquent

--- landon_app/app/Http/Controllers/ClientController.php
+++ landon_app/app/Http/Controllers/ClientController.php
@@ -8,13 +8,85 @@ use App\Title as Title;
 class ClientController extends Controller
 {
     //
-    public function __construct( Title $titles)
-    {   
+    public function __construct( Title $titles )
+    {
         $this->titles = $titles->all();
     }
 
     public function di()
     {
         dd($this->titles);
+    }
+
+    public function index()
+    {
+        $data = [];
+
+       $obj = new \stdClass;
+       $obj->id = 1;
+       $obj->title = 'mr';
+       $obj->name = 'Igor';
+       $obj->last_name = 'Silva';
+       $obj->email = 'igor.silva9813@gmail.com';
+       $data['clients'][] = $obj;
+    
+       $obj = new \stdClass;
+       $obj->id = 2;
+       $obj->title = 'ms';
+       $obj->name = 'Rita';
+       $obj->last_name = 'Pereira';
+       $obj->email = 'ritapereira@gmail.com';
+       $data['clients'][] = $obj;
+    
+       return view('client/index', $data);
+    }
+
+    public function newClient( Request $request)
+    {
+        $data = [];
+
+        $data['title'] = $request->input('title');
+        $data['name'] = $request->input('name');
+        $data['last_name'] = $request->input('last_name');
+        $data['address'] = $request->input('address');
+        $data['zip_code'] = $request->input('zip_code');
+        $data['city'] = $request->input('city');
+        $data['state'] = $request->input('state');
+        $data['email'] = $request->input('email');
+       
+        $data['titles'] = $this->titles;
+        $data['modified'] = 0;
+
+        if( $request->isMethod('POST'))
+        {
+            //dd($data);
+            $this->validate(
+               $request,
+               [
+                   'name' => 'required',
+                   'last_name' => 'required',
+                   'address' => 'required',
+                   'zip_code' => 'required',
+                   'state' => 'required',
+                   'email' => 'required',
+               ] 
+            );
+
+            return redirect('clients');
+        }
+
+        return view('client/form', $data);
+    }
+
+    public function create()
+    {
+            return view('client/create');
+    }
+
+    public function show($client_id)
+    {
+        $data['titles'] = $this->titles;
+        $data['modified'] = 1;
+        return view('client/form', $data);
     }
 }
--- landon_app/app/ReadOnlyBase.php
+++ landon_app/app/ReadOnlyBase.php
@@ -8,15 +8,14 @@ class ReadOnlyBase
 {
     //
     protected $titles_array = [];
-    
+
     public function all()
     {
         return $this->titles_array;
     }
 
-    public function get($id)
+    public function get( $id )
     {
         return $this->titles_array[$id];
     }
-
 }
